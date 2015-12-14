02d2c28a238ec0a0e830edf9ee3c8da2058153d8
Olivier Lacan <hi@olivierlacan.com>
2015-12-14 15:19:45 -0500

    Display URL to the TV model and clean output

--- muncher.rb
+++ muncher.rb
@@ -2,6 +2,8 @@ Bundler.require
 
 require "net/http"
 
+Domain = "http://www.legendmicro.com/store"
+
 Manufacturers = {
   samsung: 39
 }
@@ -31,7 +33,7 @@ end
   page_number = page + 1
 
   # get the HTML from the website
-  uri  = URI("http://www.legendmicro.com/store/594_30in-and-Up-LCD.lmcp?page=#{page_number}&Sortby=1&mfg_ID=#{Manufacturers[:samsung]}")
+  uri  = URI("#{Domain}/594_30in-and-Up-LCD.lmcp?page=#{page_number}&Sortby=1&mfg_ID=#{Manufacturers[:samsung]}")
   body = Net::HTTP.get(uri)
 
   # parse it and use CSS selectors to find all rows the big table
@@ -41,13 +43,19 @@ end
 
   # print each TV name and specs
   tvs.each do |tv|
-    info = tv.css('td')[1].text.strip.split("\r\n\t\t\t\t\t\t\t")
+    raw_info = tv.css('td')[1]
+    relative_path = raw_info.css('a').first.attributes["href"].value
+    tv_url = "#{Domain}/#{relative_path}"
+    info = raw_info.text.strip.split("\r\n\t\t\t\t\t\t\t")
     name = info[0]
     specs = info[1]
 
     next if sucks?(name, specs)
 
-    puts "#{cleaned_name(name)} \t \t #{specs.lstrip}"
+    puts "#{cleaned_name(name)}"
+    puts "#{specs.lstrip}"
+    puts "#{tv_url}"
+    puts "\n"
   end
 end
 
